a75f9cdbf19907bc11d73d0ccae3c664ca218db7
lbors <lucas_borges95@yahoo.com.br>
2019-10-22 17:53:07 -0300

    Update dct.py

--- code/dct.py
+++ code/dct.py
@@ -81,6 +81,16 @@ def transform_to_block(image, H, W):
 
     return trans_image, blocks, edges
 
+def luminance(block):
+    matrix =  "[[16,  11,  10,  16,  24,  40,  51,  61], [12,  12,  14,  19,  26,  58,  60,  55], [14,  13,  16,  24,  40,  57,  69,  56], [14,  17,  22,  29,  51,  87,  80,  62], [18,  22,  37,  56,  68, 109, 103,  77], [24,  35,  55,  64,  81, 104, 113,  92], [49,  64,  78,  87, 103, 121, 120, 101], [72,  92,  95,  98, 112, 100, 103,  99]]"
+
+    matrix_luminance = eval(matrix)
+    
+    blockq = np.around(np.divide(block, matrix_luminance))
+    blockq_inv = np.around(np.multiply(blockq, matrix_luminance))
+
+    return blockq_inv
+
 def main():
     # Read image
     img        = cv2.imread('dog.jpg', 0)
@@ -97,11 +107,17 @@ def main():
     dct_res = []
     for b in blocks:
         dct_res.append(dct2d(b))
+        
+    # Calculate the luminance for each block
+    lum_res = []
+    for l in dct_res:
+        lum_res.append(luminance(l))
 
     # Reconstruct the image
     # Calculate the inverse DCT transform for each block
     idct_res = []
-    for r in dct_res:
+    #for r in dct_res:
+    for r in lum_res:
         idct_res.append(idct2d(r))
 
     # Reconstruct the image
